f093c148062884970837eb204cdace2e3aa95928
Maxime Enguerrand <enguerrand.maxime@gmail.com>
2024-11-06 17:38:28 +0100

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -1,31 +1,39 @@
 #!/bin/bash
 
-source_file="/etc/nginx/sites-available/pve1"
-destination_file_pve2="/etc/nginx/sites-available/pve2"
-destination_file_pve3="/etc/nginx/sites-available/pve3"
-enabled_dir="/etc/nginx/sites-enabled"
+sites_enabled_dir="/etc/nginx/sites-enabled"
+sites_available_dir="/etc/nginx/sites-available"
+source_file="pve1"
+destination_file_pve2="pve2"
+destination_file_pve3="pve3"
 
-if [ ! -f "$source_file" ]; then
-    echo "Le fichier source $source_file n'existe pas."
-    exit 1
-fi
-
-cp "$source_file" "$destination_file_pve2"
-sed -i 's/pve1/pve2/g' "$destination_file_pve2"
-
-cp "$source_file" "$destination_file_pve3"
-sed -i 's/pve1/pve3/g' "$destination_file_pve3"
-
-ln -sf "$destination_file_pve2" "$enabled_dir/pve2"
-ln -sf "$destination_file_pve3" "$enabled_dir/pve3"
-
-systemctl restart nginx
-
-if systemctl is-active --quiet nginx; then
-    echo "Nginx a redémarré avec succès."
+if [ -d "$sites_enabled_dir" ]; then
+    rm -rf "$sites_enabled_dir"/*
+    echo "Contenu de $sites_enabled_dir supprimé."
 else
-    echo "Erreur lors du redémarrage de Nginx."
-    exit 1
+    echo "Le dossier $sites_enabled_dir n'existe pas."
 fi
 
-echo "Les fichiers pve2 et pve3 ont été créés et activés avec succès."
+if [ -d "$sites_available_dir" ]; then
+    rm -rf "$sites_available_dir"/*
+    echo "Contenu de $sites_available_dir supprimé."
+else
+    echo "Le dossier $sites_available_dir n'existe pas."
+fi
+
+if [ -f "$source_file" ]; then
+    # Créer les fichiers pve2 et pve3 en dupliquant pve1
+    cp "$source_file" "$destination_file_pve2"
+    cp "$source_file" "$destination_file_pve3"
+    
+    # Remplacer "pve1" par "pve2" dans le fichier pve2
+    sed -i 's/pve1/pve2/g' "$destination_file_pve2"
+    sed -i 's/pve-1/pve-2/g' "$destination_file_pve2"
+    echo "Fichier pve2 créé et modifié."
+
+    # Remplacer "pve1" par "pve3" dans le fichier pve3
+    sed -i 's/pve1/pve3/g' "$destination_file_pve3"
+    sed -i 's/pve-1/pve-3/g' "$destination_file_pve3"
+    echo "Fichier pve3 créé et modifié."
+else
+    echo "Le fichier source pve1 n'existe pas dans le répertoire actuel."
+fi
