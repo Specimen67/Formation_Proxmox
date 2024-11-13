0990766b03c55fb9f56dcc4b2bf6bea8d31c0dcc
ENGUERRAND <menguerrand@dawan.fr>
2024-11-13 15:51:30 +0100

    corrections

--- netplan/netplan.sh
+++ netplan/netplan.sh
@@ -8,8 +8,10 @@ select domaine in ocean mountain nassan; do
     fi
 done
 
-mv 00-installer-config.yaml.$domaine /etc/netplan/00-installer-config.yaml
+cp 00-installer-config.yaml.$domaine /etc/netplan/00-installer-config.yaml
 netplan apply
 
 echo "net.ipv4.ip_forward=1" | sudo tee /etc/sysctl.d/99-ipforward.conf
 sudo sysctl --system
+iptables -t nat -A POSTROUTING -o ens34 -j MASQUERADE
+apt install iptables-persistent -y
--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -14,14 +14,16 @@ case "$choix" in
         exit 1
         ;;
 esac
-cert_file="$ssl_dir/ocean.${prenom}.lan.crt"
-key_file="$ssl_dir/ocean.${prenom}.lan.key"
+
+
 sites_enabled_dir="/etc/nginx/sites-enabled"
 sites_available_dir="/etc/nginx/sites-available"
 source_file="pve1"
 destination_file_pve2="pve2"
 destination_file_pve3="pve3"
 ssl_dir="/etc/nginx/ssl"
+cert_file="$ssl_dir/$domaine.${prenom}.lan.crt"
+key_file="$ssl_dir/$domaine.${prenom}.lan.key"
 
 if [ -d "$sites_enabled_dir" ]; then
     rm -rf "$sites_enabled_dir"/*
@@ -44,12 +46,14 @@ if [ -f "$source_file" ]; then
     
     # Remplacer "pve1" par "pve2" dans le fichier pve2
     sed -i 's/pve1/pve2/g' "$destination_file_pve2"
-    sed -i 's/pve-1/pve-2/g' "$destination_file_pve2"
+    sed -i 's/px1/px2/g' "$destination_file_pve2"
+    sed -i "s/domaine/$domaine/g" "$destination_file_pve2"
+    sed -i "s/domaine/$domaine/g" "$destination_file_pve3"
     echo "Fichier pve2 créé et modifié."
 
     # Remplacer "pve1" par "pve3" dans le fichier pve3
     sed -i 's/pve1/pve3/g' "$destination_file_pve3"
-    sed -i 's/pve-1/pve-3/g' "$destination_file_pve3"
+    sed -i 's/px1/px3/g' "$destination_file_pve3"
     echo "Fichier pve3 créé et modifié."
 else
     echo "Le fichier source pve1 n'existe pas dans le répertoire actuel."
@@ -68,7 +72,7 @@ mkdir -p "$ssl_dir"
 openssl req -new -newkey rsa:2048 -days 365 -nodes -x509 \
     -keyout "$key_file" \
     -out "$cert_file" \
-    -subj "/C=FR/ST=Ile-de-France/L=Paris/O=Entreprise/OU=IT/CN=ocean.${prenom}.lan"
+    -subj "/C=FR/ST=Ile-de-France/L=Paris/O=Entreprise/OU=IT/CN=$domaine.${prenom}.lan"
 
 echo "Certificats SSL générés :"
 echo " - Certificat : $cert_file"
@@ -85,12 +89,12 @@ ln -s "$sites_available_dir/pve3" "$sites_enabled_dir/pve3"
 echo "Sites pve1, pve2 et pve3 activés."
 
 #sudo sed -i '/\.lan/d' /etc/hosts
-#echo "172.16.1.1 pve1.ocean.$prenom.lan" | sudo tee -a /etc/hosts
-#echo "172.16.1.1 pve2.ocean.$prenom.lan" | sudo tee -a /etc/hosts
-#echo "172.16.1.1 pve3.ocean.$prenom.lan" | sudo tee -a /etc/hosts
-#echo "172.16.1.10 pve-1.ocean.$prenom.lan" | sudo tee -a /etc/hosts
-#echo "172.16.1.11 pve-2.ocean.$prenom.lan" | sudo tee -a /etc/hosts
-#echo "172.16.1.12 pve-3.ocean.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.1 pve1.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.1 pve2.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.1 pve3.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.10 pve-1.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.11 pve-2.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
+#echo "172.16.1.12 pve-3.$domaine.$prenom.lan" | sudo tee -a /etc/hosts
 #echo "Entrées ajoutées dans /etc/hosts."
 
 sudo systemctl restart nginx
